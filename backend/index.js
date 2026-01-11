b33286e62e492784ccdb08f88310e1c3e4594e90
Abrar Khawar <akabrar5@gmail.com>
2026-01-11 21:03:34 +0530

    added health checkup point for uptime

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors"
+import cors from "cors";
 import { connectDB } from "./utils/connectDb.js";
 import { EventRouter } from "./routes/events.routes.js";
 import { SubscribeRouter } from "./routes/subscriber.routes.js";
@@ -8,16 +8,20 @@ import { webHookRoute } from "./routes/webhook.routes.js";
 
 const app = express();
 dotenv.config();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Backend is up and running");
 });
 
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
 app.use("/api", EventRouter);
 app.use("/api", SubscribeRouter);
-app.use('/api/webhooks' , webHookRoute)
+app.use("/api/webhooks", webHookRoute);
 
 connectDB();
 app.listen(3000, () => {
