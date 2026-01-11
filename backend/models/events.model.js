c4326f43011297c0fa4feaa320759600b2f3a5d6
Abrar Khawar <akabrar5@gmail.com>
2026-01-11 15:05:19 +0530

    schemas defined

--- backend/models/events.model.js
+++ backend/models/events.model.js
@@ -1,10 +1,18 @@
 import mongoose from "mongoose";
 
-const { Schema } = mongoose
+const { Schema } = mongoose;
 
-const eventSchema = new Schema({
-  
-})
+const eventSchema = new Schema(
+  {
+    title: String,
+    category: String,
+    venue: String,
+    dateOfEvent: {
+      type: Date,
+    },
+    eventUrl: String,
+  },
+  { timestamps: true },
+);
 
-
-export const eventModel = mongoose.model("Events" , eventSchema)
+export const eventModel = mongoose.model("Events", eventSchema);
--- backend/models/subscriber.model.js
+++ backend/models/subscriber.model.js
@@ -2,6 +2,12 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-const subscriberSchema = new Schema({});
+const subscriberSchema = new Schema(
+  {
+    email: String,
+    optIn: { type: Boolean, default: false },
+  },
+  { timestamps: true },
+);
 
 export const subscriberModel = mongoose.model("Subscribers", subscriberSchema);
